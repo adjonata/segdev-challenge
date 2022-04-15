97cceb0ff78269e9e3822f7a3b879d3c88b51ff7
adjonata <djonataalex@gmail.com>
2022-04-15 04:13:38 -0300

    refactor(routes): removing unused routes

--- src/routes/characters.routes.ts
+++ src/routes/characters.routes.ts
@@ -5,17 +5,11 @@ import { CharactersRoutes } from "./enums";
  * Components
  */
 import CharactersList from "../pages/characters/List.vue";
-import CharactersView from "../pages/characters/View.vue";
 
 export const charactersRoutes: RouteRecordRaw[] = [
   {
     path: "/characters",
     name: CharactersRoutes.LIST,
     component: CharactersList
-  },
-  {
-    path: "/characters/:id",
-    name: CharactersRoutes.VIEW,
-    component: CharactersView
   }
 ];
--- src/routes/episodes.routes.ts
+++ src/routes/episodes.routes.ts
@@ -5,17 +5,11 @@ import { EpisodeRoutes } from "./enums";
  * Components
  */
 import EpisodesList from "../pages/episodes/List.vue";
-import EpisodesView from "../pages/episodes/View.vue";
 
 export const episodesRoutes: RouteRecordRaw[] = [
   {
     path: "/episodes",
     name: EpisodeRoutes.LIST,
     component: EpisodesList
-  },
-  {
-    path: "/episodes/:id",
-    name: EpisodeRoutes.VIEW,
-    component: EpisodesView
   }
 ];
